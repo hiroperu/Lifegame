be7518f59889fb34052f98cd2e4f863f882eeee1
hiroperu <hiroki.vomit@gmail.com>
2020-04-04 23:28:25 +0900

    標準出力上で更新表示

--- Lifegame.py
+++ Lifegame.py
@@ -1,40 +1,42 @@
 import numpy as np
+import time
 import random
 import itertools
 
-ROW = 10
-COL = 10
+# import os
+
+print("")
 
 
 def init_board(row=10, col=10):
     board = np.zeros((row, col), dtype=int)
     # print([random.randint(0, 9) for i in range(10)])
-    # rand_row = [random.randint(0, row - 1) for i in range(5)]
-    # rand_col = [random.randint(0, col - 1) for i in range(5)]
-    # for i, j in itertools.product(rand_row, rand_col):
-    #     board[i][j] = True
-    #    # print(i, j)
-    board[5][5] = 1
-    board[4][5] = 1
-    board[6][5] = 1
+    rand_row = [random.randint(0, row - 1) for i in range(5)]
+    rand_col = [random.randint(0, col - 1) for i in range(5)]
+    for i, j in itertools.product(rand_row, rand_col):
+        board[i][j] = 1
+    # print(i, j)
+    # board[5][5] = 1
+    # board[4][5] = 1
+    # board[6][5] = 1
 
     return board
 
 
 def show_life(board):
     row, col = board.shape
-    for i in range(row):
-        print(i, end="")
-    print("")
+    # for i in range(row):
+    #     print(i, end="")
+    # print("")
     for i in range(row):
         for j in range(col):
             if board[i][j] == 1:
                 print("@", end="")
             else:
                 print("+", end="")
-
         print(i)
-    # print("\033[11A")
+    time.sleep(1)
+    print("\033[11F")
 
 
 def count_around(board, i, j):
@@ -54,7 +56,7 @@ def count_around(board, i, j):
 
 def next_age(board):
     row, col = board.shape
-    next_board = np.zeros((row, col), dtype=bool)
+    next_board = np.zeros((row, col), dtype=int)
     for i in range(1, row - 1):
         for j in range(1, col - 1):
             around = count_around(board, i, j)
@@ -83,7 +85,7 @@ def next_age(board):
                     next_board[i][j] = 0
                 else:
                     next_board[i][j] = 1
-
+    # print(next_board)
     return next_board
 
 
@@ -93,10 +95,13 @@ def next_age(board):
 # board[0][0] = True
 # board[1][0] = True
 board = init_board()
-show_life(board)
-board = next_age(board)
-show_life(board)
-board = next_age(board)
-show_life(board)
+# show_life(board)
+# board = next_age(board)
+# show_life(board)
+# board = next_age(board)
+# show_life(board)
+for i in range(20):
+    board = next_age(board)
+    show_life(board)
 # print(board.shape)
 # print(board)
